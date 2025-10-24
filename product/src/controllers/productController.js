13d85ff01e9a4bc0c02034da3ee9acca724e079f
NhiPNY <phannhi124c@gmail.com>
2025-10-24 19:38:50 +0700

    commit

--- product/src/controllers/productController.js
+++ product/src/controllers/productController.js
@@ -110,16 +110,7 @@ class ProductController {
     }
   }
 
-  async getProductById(req, res){
-  try{
-    if(!req.headers.authorization) return res.sendStatus(401);
-    const p=await Product.findById(res.params.id);
-    return p?res.json(p):res.sendStatus(404);
-
-  }catch {res.sendStatus(500)};  
-  }
-
-
+  
 
 }
 
